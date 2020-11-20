b86561b4cc4078cfe14f67c0f5094abd0a7099fd
Deepak Jha <deepakjha@Deepaks-MBP.lan1>
2020-11-20 11:21:50 -0800

    Changes Assignment 2

--- Assignment2/linear_regression.py
+++ Assignment2/linear_regression.py
@@ -1,12 +1,20 @@
 import numpy as np
 import pandas as pd
 
+
 ############################################################################
 # DO NOT MODIFY CODES ABOVE 
 # DO NOT CHANGE THE INPUT AND OUTPUT FORMAT
 ############################################################################
 
 ###### Part 1.1 ######
+
+def transpose_not_needed(X, w):
+    columns_X = np.size(X, 1)
+    rows_w = np.size(w, 0)
+    return columns_X == rows_w
+
+
 def mean_square_error(w, X, y):
     """
     Compute the mean square error of a model parameter w on a test set X and y.
@@ -17,27 +25,28 @@ def mean_square_error(w, X, y):
     Returns:
     - err: the mean square error
     """
-    #####################################################
-    # TODO 1: Fill in your code here                    #
-    #####################################################
-    err = None
+    prediction = np.dot(X, w) if transpose_not_needed(X, w) else np.dot(X.transpose(), w)
+    squared_error = np.square(np.subtract(prediction, y))
+    err = np.mean(squared_error, dtype=np.float64)
     return err
+
 
 ###### Part 1.2 ######
 def linear_regression_noreg(X, y):
-  """
-  Compute the weight parameter given X and y.
-  Inputs:
-  - X: A numpy array of shape (num_samples, D) containing features
-  - y: A numpy array of shape (num_samples, ) containing labels
-  Returns:
-  - w: a numpy array of shape (D, )
-  """
-  #####################################################
-  #	TODO 2: Fill in your code here                    #
-  #####################################################		
-  w = None
-  return w
+    """
+    Compute the weight parameter given X and y.
+    Inputs:
+    - X: A numpy array of shape (num_samples, D) containing features
+    - y: A numpy array of shape (num_samples, ) containing labels
+    Returns:
+    - w: a numpy array of shape (D, )
+    """
+    X_transpose = X.transpose()
+    X_transpose_X = np.dot(X_transpose, X)
+    X_transpose_X_inverse = np.linalg.inv(X_transpose_X)
+    X_transpose_y = np.dot(X_transpose, y)
+    w = np.dot(X_transpose_X_inverse, X_transpose_y)
+    return w
 
 
 ###### Part 1.3 ######
@@ -51,10 +60,11 @@ def regularized_linear_regression(X, y, lambd):
     Returns:
     - w: a numpy array of shape (D, )
     """
-  #####################################################
-  # TODO 4: Fill in your code here                    #
-  #####################################################		
-    w = None
+    X_transpose = X.transpose()
+    X_transpose_X = np.dot(X_transpose, X)
+    lambda_I = lambd * np.identity(np.size(X, 1))
+    X_transpose_y = np.dot(X_transpose, y)
+    w = np.dot(np.linalg.inv(np.add(X_transpose_X, lambda_I)), X_transpose_y)
     return w
 
 ###### Part 1.4 ######
@@ -69,12 +79,16 @@ def tune_lambda(Xtrain, ytrain, Xval, yval):
     Returns:
     - bestlambda: the best lambda you find among 2^{-14}, 2^{-13}, ..., 2^{-1}, 1.
     """
-    #####################################################
-    # TODO 5: Fill in your code here                    #
-    #####################################################		
     bestlambda = None
+    min_mse = float("inf")
+    for power in range(-14, 1):
+        lmbda = 2 ** power
+        w = regularized_linear_regression(Xtrain, ytrain, lmbda)
+        mse = mean_square_error(w, Xval, yval)
+        if mse < min_mse:
+            bestlambda = lmbda
+            min_mse = mse
     return bestlambda
-    
 
 ###### Part 1.6 ######
 def mapping_data(X, p):
@@ -86,14 +100,13 @@ def mapping_data(X, p):
     Returns:
     - X: The augmented dataset. You might find np.insert useful.
     """
-    #####################################################
-    # TODO 6: Fill in your code here                    #
-    #####################################################		
-    
+    X_temp = X
+    for power in range(2, p + 1):
+        X = np.concatenate((X, np.power(X_temp, power)), axis=1)
     return X
+
 
 """
 NO MODIFICATIONS below this line.
 You should only write your code in the above functions.
 """
-
